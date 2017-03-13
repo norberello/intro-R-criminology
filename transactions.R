f4aca4b55f9f6bf8baf0dc3a175296da66e47541
Toby Dylan Hocking <tdhock5@gmail.com>
2017-03-13 12:41:59 -0400

    Rprofile

--- transactions.R
+++ transactions.R
@@ -11,3 +11,31 @@
 ## degree centrality, flow betweenness and density.
 library(data.table)
 transactions <- fread("transactions.txt")
+setnames(transactions, c("seller", "buyer", "date.chr"))
+transactions[, date.POSIXct := strptime(date.chr, "%Y-%m-%d")]
+
+ggplot()+
+  geom_bar(aes(date.POSIXct), data=transactions)
+
+transactions[, month.chr := strftime(date.POSIXct, "%Y-%m")]
+
+ggplot()+
+  geom_bar(aes(month.chr), data=transactions)
+
+transactions[, month.POSIXct := strptime(paste0(month.chr, "-01"), "%Y-%m-%d")]
+
+ggplot()+
+  geom_bar(aes(month.POSIXct), data=transactions)
+
+per.month <- transactions[, list(
+  transactions=.N
+  ), by=list(seller, buyer, month.POSIXct)]
+seller.info <- per.month[, list(
+  buyers=.N
+  ), by=list(seller, month.POSIXct)]
+buyer.info <- per.month[, list(
+  sellers=.N
+  ), by=list(buyer, month.POSIXct)]
+per.month2 <- per.month[seller.info, on=list(
+  seller, month.POSIXct)][buyer.info, on=list(
+    buyer, month.POSIXct)]
